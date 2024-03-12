01f289da6fe187056ac1cee2df936845fbf562b3
le.michel <ptitet73@gmail.com>
2024-03-12 17:16:55 +0100

    petites améliorations de l'ui

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import customtkinter as ctk
 import numpy as np
 from simulation import Simulation
-import tkinter as tk
 from PIL import ImageTk, Image
 
 # Paper 0
@@ -12,10 +11,10 @@ class MenuFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master=master, fg_color=master.cget('fg_color'))
 
-        self.run, self.stop = None, None
+        self.run_strvar = ctk.StringVar(master=self, value='Lancer la simulation')
 
-        self.btn_run = ctk.CTkButton(master=self, text='Lancer la simulation', bg_color=self.cget('fg_color'))
-        self.btn_run.grid(padx= 20, pady=(20, 10))
+        self.btn_run = ctk.CTkButton(master=self, textvariable=self.run_strvar, bg_color=self.cget('fg_color'))
+        self.btn_run.grid(padx=20, pady=(20, 10))
         self.btn_stop = ctk.CTkButton(master=self, text='Stopper la simulation', bg_color=self.cget('fg_color'))
         self.btn_stop.grid(padx=20, pady=10)
         self.btn_reset = ctk.CTkButton(master=self, text='Réinitialiser', bg_color=self.cget('fg_color'))
@@ -40,7 +39,7 @@ class CanvasFrame(ctk.CTkFrame):
         self.canvas.grid(padx=10, pady=10)
 
 class App(ctk.CTk):
-    def __init__(self, window_title, grid_size, tile_size, pop_size, layers, sim_data):
+    def __init__(self, window_title, grid_size, tile_size, pop_size, layers, sim_data, delta_time):
         super().__init__()
 
         ctk.set_appearance_mode('dark')
@@ -63,17 +62,30 @@ class App(ctk.CTk):
             'scissors': self.load_image('assets/scissors.png', tile_size)
         }
 
-        self.sim_running = True
+        self._sim_running = True
         self.sim_grid_size = grid_size
         self.tile_size = tile_size
         self.sim_pop_size = pop_size
         self.sim_layers = layers
         self.sim_data = sim_data
+        self.sim_delta_time = delta_time
         self.sim = Simulation(tuple(grid_size), pop_size, list(layers), sim_data)
-        self.menu.on_run(self.run_sim)
+        self.menu.on_run(self.launch_sim)
         self.menu.on_stop(self.stop_sim)
         self.menu.on_reset(self.reset_sim)
     
+    @property
+    def sim_running(self):
+        return self._sim_running
+    
+    @sim_running.setter
+    def sim_running(self, value):
+        if value:
+            self.menu.run_strvar.set('Relancer la simulation')
+        else:
+            self.menu.run_strvar.set('Lancer la simulation')
+        self._sim_running = value
+
     def load_image(self, path, size):
         img = Image.open(path).resize((size, size))
         return ImageTk.PhotoImage(img)
@@ -94,28 +106,31 @@ class App(ctk.CTk):
                     img = self.assets['scissors']
                 self.canvas.canvas.create_image(i[0] * self.tile_size, i[1] * self.tile_size, image=img, anchor='nw')
     
+    def load_new_sim(self):
+        self.sim = Simulation(tuple(self.sim_grid_size), self.sim_pop_size, list(self.sim_layers), self.sim_data)  # TODO améliorer le reset
+    
     def reset_sim(self):
         self.stop_sim()
         self.clear_canvas()
-        self.sim = Simulation(tuple(self.sim_grid_size), self.sim_pop_size, list(self.sim_layers), self.sim_data) # TODO améliorer le reset
+        self.load_new_sim()
     
     def clear_canvas(self):
         self.canvas.canvas.delete('all')
     
+    def launch_sim(self):
+        self.load_new_sim()
+        self.run_sim()
+
     def run_sim(self):
         if self.sim_running:
             self.sim_running = self.sim.step()
             self.update_canvas()
-            self.after(250, self.run_sim)
+            self.after(self.sim_delta_time, self.run_sim)
         else:
             self.sim_running = True
 
     def stop_sim(self):
         self.sim_running = False
-
-    def setup_sim(self):
-        pass
-
 
 data = {
     'speed': (1, 1, 1),
@@ -127,5 +142,5 @@ data = {
     'range': (5, 5, 5)
 }
 
-app = App(window_title='PyRPS Simulation', grid_size=(30, 30), tile_size=20, pop_size=30, layers=(10, 10), sim_data=data)
+app = App(window_title='PyRPS Simulation', grid_size=(30, 30), tile_size=10, pop_size=20, layers=(10, 10), sim_data=data, delta_time=10)
 app.mainloop()
